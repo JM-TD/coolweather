5811ce71f3098de5fc6cb4e9067c19f7bf6a91cf
JM-TD <chanxb@126.com>
2015-10-06 19:33:12 +0800

    First commit.

---
+++ app/src/main/java/com/chan/coolweather/model/Province.java
@@ -0,0 +1,40 @@
+package com.chan.coolweather.model;
+
+/**
+ * Created by Chan on 2015/10/6.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getProvinceName()
+    {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName)
+    {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode()
+    {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode)
+    {
+        this.provinceCode = provinceCode;
+    }
+}
